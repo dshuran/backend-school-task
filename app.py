1de377089aa5beb4ebfd7a88643573df11d7319d
dshuran <dmitry.shuran@gmail.com>
2019-08-15 22:03:45 +0300

    POST-метод без проверок на правильность данных написан.

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\MyWorkRep\\backend-school
 db = SQLAlchemy(app)
 
 
+class ImportCounter(db.Model):
+    # SINGLETON
+    id = db.Column(db.Integer, primary_key=True)
+
+    def inc(self):
+        self.id += 1
+        print(self.id)
+        db.session.commit()
+        return self.id
+
+    def __repr__(self):
+        return 'Import Counter %r' % self.id
+
+
+import_counter = ImportCounter(id=0)
+
+
 class Dataset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
@@ -15,6 +32,7 @@ class Dataset(db.Model):
 
 
 class Citizen(db.Model):
+    # some checks on date maybe
     citizen_id = db.Column(db.Integer, primary_key=True)
     town = db.Column(db.String)
     street = db.Column(db.String)
@@ -23,7 +41,7 @@ class Citizen(db.Model):
     name = db.Column(db.String)
     birth_date = db.Column(db.DATETIME)
     gender = db.Column(db.String)
-    relatives = db.Column(db.ARRAY(db.Integer))
+    relatives = db.Column(db.Integer) # do Array
 
     dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'), primary_key=True)
     dataset = db.relationship('Dataset', backref=db.backref('citizens'))
@@ -38,11 +56,11 @@ def hello_world():
 
 
 @app.route('/imports', methods=['POST'])
-def upload_data():
-    upload_data.main()
+def import_data():
+    return upload_data.main()
 
 
 if __name__ == '__main__':
-    # db.create_all()
+    db.create_all()
     app.run(debug=True)
 
--- upload_data.py
+++ upload_data.py
@@ -1,6 +1,27 @@
-from flask import request, abort
+from flask import request, abort, jsonify
+from app import Citizen, import_counter
 
 
 def main():
-    if not request.json:
+    if not request.json:  # more checks
         abort(400)
+    citizens = request.json['citizens']
+    for citizen_obj in citizens:
+        # there check for every citizen obj
+        citizen = Citizen(
+            citizen_id=citizen_obj['citizen_id'],  # check, что нет такого же айди в выгрузке
+            town=citizen_obj['town'],
+            street=citizen_obj['street'],
+            building=citizen_obj['building'],
+            apartment=citizen_obj['apartment'],
+            name=citizen_obj['name'],
+            birth_date=citizen_obj['birth_date'],
+            gender=citizen_obj['gender'],
+            relatives=citizen_obj['relatives'])
+        # add to db
+    success_response = {
+        "data": {
+            "import_id": import_counter.inc()
+        }
+    }
+    return jsonify(success_response), 201
