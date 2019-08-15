9c981537bc0ba73152f4db42516a0241bbca7758
dshuran <dmitry.shuran@gmail.com>
2019-08-15 18:34:55 +0300

    Созданы базовые классы выгрузки данных и жителя.

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\MyWorkRep\\backend-school
 db = SQLAlchemy(app)
 
 
+class Dataset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    children = db.relationship('Citizen')
+
+
+class Citizen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'))
+    dataset = db.relationship('Dataset')
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
