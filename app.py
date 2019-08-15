e9946a8d9e7a48814d4689c90e087b8ee4e707c3
dshuran <dmitry.shuran@gmail.com>
2019-08-15 18:23:39 +0300

    Написаны основы базы данных.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\MyWorkRep\\backend-school-task\\database.dt'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
