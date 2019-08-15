29856e7e985b4a69b0895f81d9b25cd70c287303
dshuran <dmitry.shuran@gmail.com>
2019-08-15 19:26:46 +0300

    Реализовали схему с уникальными айдишниками пользователей в каждой выгрузке (в каждой новой нумерация айдишников будет снова начинаться c 1-цы)

--- app.py
+++ app.py
@@ -5,16 +5,29 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\MyWorkRep\\backend-school-task\\database.dt'
 db = SQLAlchemy(app)
 
+citizen_id = 0
+
+
+def inc_citizen_id():
+    global citizen_id
+    citizen_id += 1
+    return citizen_id
+
 
 class Dataset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    children = db.relationship('Citizen')
+
+    def __repr__(self):
+        return 'Dataset %r' % self.id
 
 
 class Citizen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'))
-    dataset = db.relationship('Dataset')
+    dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'), primary_key=True)
+    dataset = db.relationship('Dataset', backref=db.backref('citizens'))
+
+    def __repr__(self):
+        return 'Citizen %r cit_id %r' % (self.dataset, self.id)
 
 
 @app.route('/')
@@ -23,4 +36,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    # db.create_all()
     app.run(debug=True)
+
