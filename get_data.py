d0f2e321ddaa735f8e5617a53dfdb39189e72a9a
dshuran <dmitry.shuran@gmail.com>
2019-08-18 13:34:02 +0300

    Обработали исключение, если неправильный id в get_data

--- get_data.py
+++ get_data.py
@@ -1,17 +1,20 @@
 from app import Dataset
-from flask import jsonify
+from flask import jsonify, abort
 
 
 def main(import_id):
-    dataset = Dataset.query.filter_by(id=import_id).first()
-    if dataset is None:
-        raise ValueError
-    else:
-        res_list = []
-        for citizen in dataset.citizens:
-            res_list.append(citizen.json_representation())
-        res = {
-            "data": res_list
-        }
-        # todo: вывод без переводов строки. Скорее всего уберётся в production-версии.
-        return jsonify(res), 200
+    try:
+        dataset = Dataset.query.filter_by(id=import_id).first()
+        if dataset is None:
+            raise ValueError
+        else:
+            res_list = []
+            for citizen in dataset.citizens:
+                res_list.append(citizen.json_representation())
+            res = {
+                "data": res_list
+            }
+            # todo: вывод без переводов строки. Скорее всего уберётся в production-версии.
+            return jsonify(res), 200
+    except ValueError:
+        abort(400)
