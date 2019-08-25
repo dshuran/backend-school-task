e5391da1a052315d7d0ec290a33c3bab217b2182
Dmitry Shuran <shud@1c.ru>
2019-08-25 21:29:21 +0300

    Фикс хоста.

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_exception(e):
 def main():
     if not os.path.isfile(db_path):
         setup_database()
-    app.run(host='0:0:0:0', port='8080')
+    app.run(host='0.0.0.0', port='8080')
 
 
 if __name__ == '__main__':
